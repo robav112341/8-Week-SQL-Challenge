ad538f128fcc622ef25cc0efaa7aeb405c12ff9f
robav112341 <49269011+robav112341@users.noreply.github.com>
2023-09-20 22:40:15 +0300

    Update Case Study #3  Foodie-Fi Solution.sql

--- 8 Week Challange/ex 3/Case Study #3  Foodie-Fi Solution.sql
+++ 8 Week Challange/ex 3/Case Study #3  Foodie-Fi Solution.sql
@@ -8,7 +8,7 @@ FROM
 -- Question 2:
 -- What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
 SELECT 
-    MONTHNAME(start_date) AS month_, COUNT(customer_id)
+    MONTHNAME(start_date) AS month_, COUNT(customer_id) AS count_customers
 FROM
     subscriptions
 WHERE
@@ -21,7 +21,7 @@ ORDER BY MONTH(start_date);
 SELECT 
     s.plan_id,
     p.plan_name,
-    COUNT(s.customer_id) AS costomer_count
+    COUNT(s.customer_id) AS customer_count
 FROM
     subscriptions s
         JOIN
@@ -71,13 +71,16 @@ WITH cte AS (
     FROM
 		subscriptions)
 SELECT 
-current_plan,
-	COUNT(*) AS churn_count,
-	ROUND(COUNT(*)/(SELECT COUNT(DISTINCT customer_id) FROM subscriptions)*100,1) AS churn_precentege
+	p.plan_name,
+	COUNT(*) AS count,
+	ROUND(COUNT(*)/(SELECT COUNT(DISTINCT customer_id) FROM subscriptions)*100,1) AS percentage_of_total  
 FROM
-	cte
+	cte c
+JOIN
+	plans p ON c.current_plan = p.plan_id
 WHERE pre_plan = 0
-GROUP BY current_plan;
+GROUP BY 1
+ORDER BY 1;
 
 -- Question 7
 -- What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
@@ -92,12 +95,15 @@ WITH cte AS (
 		start_date < '2020-12-31'
 	GROUP BY customer_id)
 SELECT
-	max_plan as plan,
-	COUNT(customer_id) as count_p,
-	ROUND(COUNT(customer_id)/(SELECT COUNT(DISTINCT customer_id) FROM subscriptions)*100,1) AS pracentage
+	p.plan_name,
+	COUNT(c.customer_id) as count,
+	ROUND(COUNT(c.customer_id)/(SELECT COUNT(DISTINCT customer_id) FROM subscriptions)*100,1) AS pracentage
 FROM
-	cte
-GROUP BY max_plan;
+	cte c
+JOIN 
+	plans p ON c.max_plan = p.plan_id
+GROUP BY max_plan
+ORDER BY 1;
 
 -- Question 8
 -- How many customers have upgraded to an annual plan in 2020?
@@ -110,7 +116,8 @@ WITH cte AS(
 		subscriptions
 	WHERE  start_date < '2020-12-31')
 SELECT
-	COUNT(*) 
+	(SELECT plan_name FROM plans WHERE plan_id = 3) as plan_name,
+	COUNT(*) count_annual_upgrades
 FROM
 	cte 
 WHERE plan_id = 3 AND p_plan IS NOT NULL;
@@ -131,6 +138,7 @@ now_date AS (
     FROM subscriptions 
     WHERE plan_id = 3)
 SELECT
+	(SELECT plan_name FROM plans WHERE plan_id = 3) as plan_name,	
 	ROUND(AVG(nd.now_date - md.min_date),0) AS time_p
 FROM now_date nd
 JOIN min_date md ON nd.customer_id = md.customer_id;
@@ -140,9 +148,10 @@ JOIN min_date md ON nd.customer_id = md.customer_id;
  WITH min_date AS (
 	SELECT 
 		customer_id,
-		MIN(start_date) AS min_date
+		start_date AS min_date
 	FROM
 		subscriptions
+	 WHERE plan_id = 0
 	GROUP BY customer_id),
 now_date AS (
 	SELECT 
@@ -151,20 +160,40 @@ now_date AS (
     FROM subscriptions 
     WHERE plan_id = 3)
 SELECT
+(SELECT plan_name FROM plans WHERE plan_id = 3) AS plan_name,
 CASE
 	WHEN nd.now_date - md.min_date < 31 THEN '0-30'
 	WHEN nd.now_date - md.min_date BETWEEN 31 AND 60 THEN '31-60'
     WHEN nd.now_date - md.min_date BETWEEN 61 AND 90 THEN '61-90'
-    ELSE '90+' END AS the_time_pass,
-	ROUND(AVG(nd.now_date - md.min_date),0) AS time_p_AVG,
-	COUNT(*) AS costumer_count
+    WHEN nd.now_date - md.min_date BETWEEN 91 AND 120 THEN '91-120'
+    WHEN nd.now_date - md.min_date BETWEEN 121 AND 150 THEN '121-150'
+    WHEN nd.now_date - md.min_date BETWEEN 151 AND 180 THEN '151-180'
+    WHEN nd.now_date - md.min_date BETWEEN 181 AND 210 THEN '181-210'
+    WHEN nd.now_date - md.min_date BETWEEN 211 AND 240 THEN '211-240'
+    WHEN nd.now_date - md.min_date BETWEEN 241 AND 271 THEN '241-270'
+    WHEN nd.now_date - md.min_date BETWEEN 271 AND 300 THEN '271-300'
+    WHEN nd.now_date - md.min_date BETWEEN 301 AND 330 THEN '301-330'
+    WHEN nd.now_date - md.min_date BETWEEN 331 AND 360 THEN '331-360'
+    ELSE '360+' END AS the_time_pass,
+	COUNT(*) AS costumer_count,
+    ROUND(AVG(nd.now_date - md.min_date),0) AS time_p_AVG
 FROM now_date nd
 JOIN min_date md ON nd.customer_id = md.customer_id
 GROUP BY the_time_pass
 ORDER BY CASE	
 	WHEN the_time_pass = '0-30' THEN 1
-	WHEN the_time_pass = '61-90' THEN 2
-ELSE 3 END;
+    WHEN the_time_pass = '31-60' THEN 2
+	WHEN the_time_pass = '61-90' THEN 3
+    WHEN the_time_pass = '91-120' THEN 4
+    WHEN the_time_pass = '121-150' THEN 5
+    WHEN the_time_pass = '151-180' THEN 6
+    WHEN the_time_pass = '181-210' THEN 7
+    WHEN the_time_pass = '211-240' THEN 8
+    WHEN the_time_pass = '241-270' THEN 9
+    WHEN the_time_pass = '271-300' THEN 10
+    WHEN the_time_pass = '301-330' THEN 11
+	WHEN the_time_pass = '331-360' THEN 12
+	WHEN the_time_pass = '360+' THEN 13 END;
 
 -- Qustion 11
 -- How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
