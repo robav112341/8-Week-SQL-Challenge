3b97273274135f2e628d9b594e6aed21a15b99d9
robav112341 <49269011+robav112341@users.noreply.github.com>
2023-10-26 21:08:58 +0300

    Update Case Study #2 - Pizza Runner A+B Solution.sql

--- 8 Week Challange/ex 2/Case Study #2 - Pizza Runner A+B Solution.sql
+++ 8 Week Challange/ex 2/Case Study #2 - Pizza Runner A+B Solution.sql
@@ -76,54 +76,89 @@ WITH cte AS(
  
 -- Question 7:
 -- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
- WITH pizza_changed AS (SELECT 
-    customer_id, COUNT(order_id) as changed_p
-FROM
-    customer_orders 
-WHERE
-    LENGTH(extras) > 0
-        OR LENGTH(exclusions) > 0
-        AND order_id IN (SELECT 
-            order_id
-        FROM
-            runner_orders
-        WHERE
-            cancellation NOT IN ('Restaurant Cancellation' , 'Customer Cancellation')
-                OR cancellation IS NULL)
-GROUP BY 1),
-no_change AS (SELECT customer_id, COUNT(*) as no_change FROM customer_orders WHERE LENGTH(extras) = 0
-        AND LENGTH(exclusions) = 0 GROUP BY 1 ORDER BY 1)
+ WITH changed_cte AS (
+	SELECT 
+		co.customer_id,
+		SUM(CASE
+			WHEN
+				(co.extras IS NOT NULL
+					AND co.extras <> 'null'
+					AND co.extras <> '')
+					OR (co.exclusions IS NOT NULL
+					AND co.exclusions <> 'null'
+					AND exclusions <> '')
+			THEN
+				1
+			ELSE 0
+		END) AS changed
+	FROM
+		customer_orders co
+	JOIN
+    runner_orders ro ON co.order_id = ro.order_id
+	WHERE
+		distance <> 'null'
+	GROUP BY 1
+	ORDER BY 1),
+unchanged_cte AS (
+	SELECT 
+		co.customer_id,
+		SUM(CASE
+			WHEN extras = '' AND exclusions = '' THEN 1
+			WHEN extras = '' AND exclusions = 'null' THEN 1
+			WHEN extras = 'null' AND exclusions = 'null' THEN 1
+			WHEN extras = 'null' AND exclusions = '' THEN 1
+			WHEN extras IS NULL AND exclusions = '' THEN 1
+			WHEN extras = '' AND exclusions IS NULL THEN 1
+			ELSE 0
+		END) AS unchanged
+	FROM
+		customer_orders co
+	JOIN
+		runner_orders ro ON co.order_id = ro.order_id
+	WHERE distance <> 'null'
+	GROUP BY 1
+	ORDER BY 1),
+combined_cte AS (
+	SELECT
+		cc.customer_id,
+		cc.changed,
+		uc.unchanged
+	FROM
+		changed_cte cc
+	JOIN
+		unchanged_cte uc ON cc.customer_id = uc.customer_id)
 SELECT
-pc.customer_id,
-pc.changed_p,
-(SELECT CASE WHEN nc.no_change IS NULL THEN 0
-ELSE nc.no_change END) as no_change
-FROM pizza_changed pc
-LEFT JOIN no_change nc ON pc.customer_id = nc.customer_id
-ORDER BY 1;
+	cc.*
+FROM
+	combined_cte cc;
 
 -- Question 8:
--- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- How many pizzas were delivered that had both exclusions and extras?
 SELECT 
-    COUNT(*) extars_and_exclusions
+    COUNT(*) AS exatras_and_exclusions
 FROM
     customer_orders co
-        JOIN
-    runner_orders ro ON co.order_id = ro.order_id
+JOIN
+	runner_orders ro ON co.order_id = ro.order_id
 WHERE
-    LENGTH(co.extras) > 0
-        AND LENGTH(co.exclusions) > 0
-        AND (ro.cancellation NOT IN ('Restaurant Cancellation' , 'Customer Cancellation')
-        OR ro.cancellation IS NULL);
+    LENGTH(co.exclusions) > 0
+        AND LENGTH(co.extras) > 0
+        AND co.exclusions <> 'null'
+        AND co.extras <> 'null'
+        AND ro.distance <> 'null';
 
 -- Question 9
 -- What was the total volume of pizzas ordered for each hour of the day?
 SELECT 
-    HOUR(order_time), COUNT(*)
+    HOUR(co.order_time) AS hours , COUNT(*) as count_volume
 FROM
-    customer_orders
+    customer_orders co
+JOIN
+	runner_orders ro ON co.order_id = ro.order_id
+WHERE
+	ro.distance <> 'null'
 GROUP BY 1
-ORDER BY 1;
+ORDER BY 2 DESC, 1;
 
 -- Question 10
 -- What was the volume of orders for each day of the week?
@@ -143,21 +178,27 @@ SELECT
     COUNT(*) AS order_count
 FROM
     customer_orders
-GROUP BY DAYOFWEEK(order_time);
+GROUP BY DAYOFWEEK(order_time)
+ORDER BY 2 DESC;
 
 -- B.Runner and Customer Experience:
 -- Question 1:
--- What is the successful delivery percentage for each runner?
+-- How many runners signed up for each 1 week period?
 SELECT 
-    COUNT(*) AS count_runners,
-    CONCAT('Week', ' ', WEEK(registration_date)) AS Week
+	CONCAT('Week', ' ', FLOOR((DAY(registration_date) - 1) / 7) + 1) AS Week,
+    COUNT(*) AS count_runners
 FROM
     runners
-GROUP BY WEEK(registration_date);
+GROUP BY FLOOR((DAY(registration_date) - 1) / 7) + 1;
 
 -- Question 2:
 -- What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
-WITH cte AS (SELECT * FROM customer_orders GROUP BY order_id)
+WITH cte AS (
+	SELECT
+		* 
+	FROM 
+		customer_orders
+    GROUP BY order_id)
 SELECT
 	ro.runner_id,
    - ROUND(AVG(timestampdiff(MINUTE,ro.pickup_time,c.order_time)),1) AS avg_time
@@ -166,18 +207,29 @@ FROM
         JOIN
     runner_orders ro ON c.order_id = ro.order_id
 WHERE
-    timestampdiff(MINUTE,ro.pickup_time,c.order_time) IS NOT NULL
-GROUP BY 1; 
+    distance <> 'null'
+GROUP BY 1;
 
 -- Question 3:
 -- Is there any relationship between the number of pizzas and how long the order takes to prepare?
-WITH cte AS (SELECT * FROM customer_orders GROUP BY order_id),
-pizza_count AS (SELECT order_id, COUNT(*) AS pizza_number FROM customer_orders GROUP BY 1)
+WITH cte AS (
+	SELECT 
+		* 
+	FROM 
+		customer_orders
+    GROUP BY order_id),
+pizza_count AS (
+	SELECT
+		order_id,
+		COUNT(*) AS pizza_count
+    FROM 
+		customer_orders
+    GROUP BY 1)
 SELECT
 	c.order_id,
-   - ROUND((timestampdiff(MINUTE,ro.pickup_time,c.order_time)),1) AS avg_time,
-    pc.pizza_number,
-	-(ROUND((timestampdiff(MINUTE,ro.pickup_time,c.order_time)/pc.pizza_number),1)) AS time_per_pizza
+   - ROUND((timestampdiff(MINUTE,ro.pickup_time,c.order_time)),1) AS avg_pickup_time,
+    pc.pizza_count,
+	-(ROUND((timestampdiff(MINUTE,ro.pickup_time,c.order_time)/pc.pizza_count),1)) AS time_per_pizza
 FROM
     cte c
         JOIN
