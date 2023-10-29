da5b0c77fbaac8a632ce9f692fffe38e30d6312b
robav112341 <49269011+robav112341@users.noreply.github.com>
2023-10-29 17:50:45 +0200

    Update Case Study #2 - Pizza Runner C Solution.sql

--- 8 Week Challange/ex 2/Case Study #2 - Pizza Runner C Solution.sql
+++ 8 Week Challange/ex 2/Case Study #2 - Pizza Runner C Solution.sql
@@ -27,6 +27,30 @@ JOIN
 pizza_names pn ON st.pizza_id = pn.pizza_id
 GROUP BY st.pizza_id;
 
+-- other way:
+DROP TABLE IF EXISTS pizza_help;
+CREATE TABLE pizza_help (
+    pizza_id INT,
+    topping_id INT,
+    PRIMARY KEY (pizza_id, topping_id)
+);
+
+INSERT INTO pizza_help (pizza_id, topping_id) VALUES
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 8), (1, 10),
+(2, 4), (2, 6), (2, 7), (2, 9), (2, 11), (2, 12);
+
+SELECT 
+     p.pizza_name,
+    GROUP_CONCAT(t.topping_name SEPARATOR ', ') as recipie
+FROM 
+    pizza_names p
+JOIN 
+    pizza_help h ON p.pizza_id = h.pizza_id
+JOIN 
+    pizza_toppings t ON h.topping_id = t.topping_id
+GROUP BY 
+    p.pizza_name;
+    
 -- Question 2:
 -- What was the most commonly added extra?
 
@@ -88,166 +112,58 @@ ORDER BY COUNT(*) DESC;
 #Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 SELECT 
     co.order_id,
-    co.pizza_id,
-    (CASE
-        WHEN
-            (LENGTH(co.extras) = 0
-                AND LENGTH(co.exclusions) = 0
-                OR (co.extras = 'null'
-                AND co.exclusions = 'null')
-                OR (LENGTH(co.extras) = 0
-                AND co.exclusions IS NULL)
-                OR (co.extras IS NULL
-                AND LENGTH(co.exclusions) = 0))
-        THEN
-            pn.pizza_name
-        WHEN
-            ((LENGTH(co.extras) = 1
-                AND LENGTH(co.exclusions) = 0)
-                OR (LENGTH(co.extras) = 1
-                AND co.exclusions = 'null'))
-        THEN
-            CONCAT(pn.pizza_name,
-                    ' ',
-                    'extra',
-                    ' ',
-                    (SELECT 
-                            topping_name
-                        FROM
-                            pizza_toppings
-                        WHERE
-                            topping_id = co.extras))
-        WHEN
-            ((LENGTH(co.extras) = 0
-                AND LENGTH(co.exclusions) = 1)
-                OR (LENGTH(co.exclusions) = 1
-                AND co.extras = 'null'))
-        THEN
-            CONCAT(pn.pizza_name,
-                    ' ',
-                    'exclude',
-                    ' ',
-                    (SELECT 
-                            topping_name
-                        FROM
-                            pizza_toppings
-                        WHERE
-                            topping_id = co.exclusions))
-        WHEN
-            ((LENGTH(co.extras) > 1
-                AND LENGTH(co.exclusions) = 1))
-        THEN
-            CONCAT(pn.pizza_name,
-                    ' ',
-                    'extra',
-                    ' ',
-                    (WITH RECURSIVE
-  unwound AS (
-    SELECT order_id, extras
-      FROM customer_orders
-    UNION ALL
-    SELECT order_id, regexp_replace(extras, '^[^,]*,', '') extras
-      FROM unwound
-      WHERE extras LIKE '%,%'
-  ),
-  separate_extras AS(
-  SELECT order_id, regexp_replace(extras, ',.*', '') extras
-    FROM unwound
-    ORDER BY order_id),
-for_concat AS (SELECT
-*
-FROM
-separate_extras
-WHERE order_id = 9 AND extras <> 'null')
-SELECT
-GROUP_CONCAT(pt.topping_name) as toppings
-FROM
-for_concat fc
-JOIN
-pizza_toppings pt ON pt.topping_id = fc.extras),' exclude ', (WITH RECURSIVE
-  unwound AS (
-    SELECT order_id, exclusions
-      FROM customer_orders
-    UNION ALL
-    SELECT order_id, regexp_replace(exclusions, '^[^,]*,', '') exclusions
-      FROM unwound
-      WHERE exclusions LIKE '%,%'
-  ),
-  separate_exclusions AS(
-  SELECT order_id, regexp_replace(exclusions, ',.*', '') exclusions
-    FROM unwound
-    ORDER BY order_id),
-for_concat AS (SELECT
-*
-FROM
-separate_exclusions 
-WHERE order_id = 9 AND exclusions <> 'null')
-SELECT
-GROUP_CONCAT(pt.topping_name) as toppings
-FROM
-for_concat fc
-JOIN
-pizza_toppings pt ON pt.topping_id = fc.exclusions))
-WHEN
-            ((LENGTH(co.extras) > 1
-                AND LENGTH(co.exclusions) > 1))
-        THEN
-            CONCAT(pn.pizza_name,
-                    ' ',
-                    'extra',
-                    ' ',
-                    (WITH RECURSIVE
-  unwound AS (
-    SELECT order_id, extras
-      FROM customer_orders
-    UNION ALL
-    SELECT order_id, regexp_replace(extras, '^[^,]*,', '') extras
-      FROM unwound
-      WHERE extras LIKE '%,%'
-  ),
-  separate_extras AS(
-  SELECT order_id, regexp_replace(extras, ',.*', '') extras
-    FROM unwound
-    ORDER BY order_id),
-for_concat AS (SELECT
-*
-FROM
-separate_extras
-WHERE order_id = 10 AND extras <> 'null')
-SELECT
-GROUP_CONCAT(pt.topping_name) as toppings
-FROM
-for_concat fc
-JOIN
-pizza_toppings pt ON pt.topping_id = fc.extras),' exclude ', (WITH RECURSIVE
-  unwound AS (
-    SELECT order_id, exclusions
-      FROM customer_orders
-    UNION ALL
-    SELECT order_id, regexp_replace(exclusions, '^[^,]*,', '') exclusions
-      FROM unwound
-      WHERE exclusions LIKE '%,%'
-  ),
-  separate_exclusions AS(
-  SELECT order_id, regexp_replace(exclusions, ',.*', '') exclusions
-    FROM unwound
-    ORDER BY order_id),
-for_concat AS (SELECT
-*
-FROM
-separate_exclusions 
-WHERE order_id = 10 AND exclusions <> 'null')
-SELECT
-GROUP_CONCAT(pt.topping_name) as toppings
-FROM
-for_concat fc
-JOIN
-pizza_toppings pt ON pt.topping_id = fc.exclusions))
-        ELSE '0'
-    END) AS pizza_name
+    CONCAT(pn.pizza_name,
+            CASE
+                WHEN co.extras = '' THEN ''
+                WHEN co.extras = 'null' THEN ''
+                WHEN co.extras IS NULL THEN ''
+                ELSE CONCAT(' Extra ', (
+                WITH RECURSIVE
+				  unwound AS (
+					SELECT  extras
+					UNION ALL
+					SELECT regexp_replace(extras, '^[^,]*,', '') exclusions
+					  FROM unwound
+					  WHERE extras LIKE '%,%'
+				  ),
+				  separate_extras AS(
+				  SELECT regexp_replace(extras, ',.*', '') exclusions
+					FROM unwound)
+				SELECT
+				group_concat(pt.topping_name)
+				FROM 
+				separate_extras se
+				JOIN
+				pizza_toppings pt ON se.exclusions = pt.topping_id)) END,
+                CASE
+                WHEN co.exclusions = '' THEN ''
+                WHEN co.exclusions = 'null' THEN ''
+                WHEN co.exclusions IS NULL THEN ''
+                ELSE CONCAT(' Exlude ', (
+                WITH RECURSIVE
+				  unwound AS (
+					SELECT  exclusions
+					UNION ALL
+					SELECT regexp_replace(exclusions, '^[^,]*,', '') exclusions
+					  FROM unwound
+					  WHERE exclusions LIKE '%,%'
+				  ),
+				  separate_exclusions AS(
+				  SELECT regexp_replace(exclusions, ',.*', '') exclusions
+					FROM unwound)
+				SELECT
+				group_concat(pt.topping_name)
+				FROM 
+				separate_exclusions se
+				JOIN
+				pizza_toppings pt ON se.exclusions = pt.topping_id)) END) as pizza_name
 FROM
     customer_orders co
         JOIN
-    pizza_names pn ON co.pizza_id = pn.pizza_id;
+    pizza_names pn ON co.pizza_id = pn.pizza_id
+        LEFT JOIN
+    pizza_toppings pt ON co.extras = pt.topping_id
+        LEFT JOIN
+    pizza_toppings pt1 ON co.exclusions = pt1.topping_id;
 
 -- Question 5+6 work in progress 
